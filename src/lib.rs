21ca5e9ac9452bfc1281868e84c486ca37f601ad
Hendra Adi <hendraadi011@gmail.com>
2025-07-21 06:00:24 +0700

    update version to 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,14 @@ pub fn say_hello(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
+pub fn say_hello_to_everyone() -> String {
+    "Hello everyone".to_string()
+}
+
 pub fn say_goodbye(name: &str) -> String {
     format!("Goodbye, {}!", name)
+}
+
+pub fn say_goodby_to_everyone() -> String {
+    "Say goodby everyone".to_string()
 }
