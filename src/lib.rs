6ad71d4f2505e65bb935bbc0ce4ba99c648ecf91
Hendra Adi <hendraadi011@gmail.com>
2025-07-21 06:12:34 +0700

    update version

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,20 @@
+#[cfg(feature = "hello")]
 pub fn say_hello(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
+#[cfg(feature = "hello")]
 pub fn say_hello_to_everyone() -> String {
     "Hello everyone".to_string()
 }
 
+#[cfg(feature = "bye")]
+
 pub fn say_goodbye(name: &str) -> String {
     format!("Goodbye, {}!", name)
 }
+
+#[cfg(feature = "bye")]
 
 pub fn say_goodby_to_everyone() -> String {
     "Say goodby everyone".to_string()
